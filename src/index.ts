da64bcaeb6f8baf20abc9177cddce35ab069f4b5
Lars Juel Jensen <larsjueljens@gmail.com>
2024-01-19 12:35:01 +0100

    Updated package.json with correct license, and some other minor changes..

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,1 @@
 import './pdf-viewer';
-import * as pdfjs from "pdfjs-dist";
-
-pdfjs.GlobalWorkerOptions.workerSrc = "dist/pdf.worker.bundle.js";
--- webpack.config.js
+++ webpack.config.js
@@ -3,10 +3,10 @@ const path = require('path');
 module.exports = {
     mode: 'development',
     entry: {
-        main: './src/index.ts'
+        "pdf-viewer": './src/index.ts'
     },
     output: {
-        filename: '[name].bundle.js',
+        filename: '[name].js',
         path: path.resolve(__dirname, 'dist'),
         publicPath: "/"
     },
